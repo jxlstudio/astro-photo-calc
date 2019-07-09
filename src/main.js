95cd8d1bd21a72f0dca41f8fc1201fea3d5cb430
jxlstudio <jeremy@jxl.io>
2019-07-09 12:04:28 -0400

    added sentry error reporting

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import Buefy from 'buefy'
 import './registerServiceWorker'
+import * as Sentry from '@sentry/browser'
+import * as Integrations from '@sentry/integrations'
+
+Sentry.init({
+  dsn: 'https://22e3f51844a248c4878e72c887a72fe8@sentry.io/1500618',
+  integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+})
 
 Vue.use(Buefy)
 
